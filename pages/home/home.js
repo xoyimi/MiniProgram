cc6eb3d788ad51565aa9cbb675cdaae64cf01fa1
xoyimi <xoyimi@foxmail.com>
2019-10-30 00:01:52 +0800

    小程序展示弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,40 @@
 Page({
- 
+  handleToastClick() {
+    wx.showToast({
+      title: 'this is toast',
+      duration: 3000,
+      icon: "loading"
+    })
+  },
+  handleModalClick() {
+    wx.showModal({
+      title: "this is Modal's title",
+      content: "this is Modal's content",
+      cancelColor: "red",
+      cancelText: "返回",
+      success(res) {
+        if (res.cancel) {
+          console.log("用户点击了取消")
+        }
+        if (res.confirm) {
+          console.log("用户点击了确认")
+        }
+      }
+    })
+  },
+  handleLoadingClick() {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
+  },
+  handleActionSheetClick(){
+    wx.showActionSheet({
+      itemList: ['相册','拍摄'],
+      itemColor:"red",
+      success(res){
+        console.log(res)
+      }
+    })
+  }
 })
